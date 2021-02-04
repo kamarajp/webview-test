a66cbd93ff7319413a1c8f3f1fd9a5d32fe80c96
Kamaraj P <kamaraju.pvv@toppr.com>
2021-02-04 19:31:01 +0530

    added katex

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-const url = 'https://new-upgrade.toppr.com/api-ask/v1/questions/what-is-the-best-way-to-test-the-relatedness-of-two-species/?format=json';
-(function(){
-    fetch(url).then(res => console.log(res))
-})();
+const qdiv = document.querySelectorAll("div[data-src-type='question']");
+Object.values(qdiv).map(q => {
+    const qslug = q.getAttribute('data-src-id');
+    const qUrl = `https://new-upgrade.toppr.com/api-ask/v1/questions/${qslug}/?format=json`;
+    fetch(qUrl).then(res => res.json()).then(res => {
+        if (res) {
+            const { data = {} } = res;
+            const questionData = data.question_data.haygot_question_info.question;
+            const answersData = data.question_data.haygot_question_info.solution;
+            q.innerHTML = `
+                <span>${questionData}</span>
+                <span>${answersData}</span>`;
+            renderMathInElement(q, {
+                delimiters: [
+                    { left: '$$', right: '$$', display: false },
+                    { left: '\\(', right: '\\)', display: false },
+                    { left: '\\[', right: '\\]', display: true }
+                ],
+                mathJaxDelimiters: [
+                    { left: '\\matrix{', right: '}', leftReplace: '\\begin{matrix}', rightReplace: '\\end{matrix}' },
+                    { left: '\\eqalign{', right: '}', leftReplace: '\\begin{aligned}', rightReplace: '\\end{aligned}' },
+                    { left: '^\\left(', right: '\\right)', leftReplace: '^{\\left(', rightReplace: '\\right)}' },
+                    { left: '^\\left[', right: '\\right]', leftReplace: '^{\\left[', rightReplace: '\\right]}' },
+                    { left: '^\\left\\{', right: '\\right\\}', leftReplace: '^{\\left\\{', rightReplace: '\\right\\}}' },
+                    { left: '\\text{', right: '}', leftReplace: '\\text{', rightReplace: '}', textRegex: [{ regex: /\$|\\\$/g, subst: '\\$' }], mathRegex: [{ regex: /\&|\\\&/g, subst: '\\&' }] }, // this must be last
+                ]
+            });
+        }
+    })
+})
