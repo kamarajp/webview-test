a970570de82e08273a994d18ddc67b7142d5db9d
Kamaraj P <kamaraju.pvv@toppr.com>
2021-02-04 17:49:29 +0530

    removed cors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const url = 'https://new-upgrade.toppr.com/api-ask/v1/questions/what-is-the-best-way-to-test-the-relatedness-of-two-species/?format=json';
 (function(){
-    fetch(url, {mode: 'no-cors'}).then(res => console.log(res))
+    fetch(url).then(res => console.log(res))
 })();
